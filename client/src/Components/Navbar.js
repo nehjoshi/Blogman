95fd6aaccfc6a379e5cd0067cc846bc79076dd88
Neh Joshi <nehjoshi5@gmail.com>
2021-06-07 15:54:27 +0530

    Added Sidebar

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import IconButton from '@material-ui/core/IconButton';
@@ -6,9 +6,35 @@ import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import favicon from '../favicon1.png';
+import ClearAllIcon from '@material-ui/icons/ClearAll';
+import {makeStyles, useTheme} from '@material-ui/core/styles'
+import {Grid, Typography} from '@material-ui/core';
+import Sidebar from './Sidebar';
 import '../App.css';
+
+const useStyles = makeStyles((theme) => ({
+    navExpand: {
+        display: 'none',
+        [theme.breakpoints.down('md')]: {
+            display: 'block'
+        },
+        '&:hover': {
+            cursor: 'pointer'
+        }
+    }
+}))
+
 const Navbar = () => {
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+    const theme = useTheme();
+
+    const handleOpen = () => {
+        setOpen(!open);
+    }
+
     return (
+        <>
             <AppBar position="fixed">
                 <Toolbar style={{ background: 'rgb(42, 42, 68)' }}>
                     <IconButton edge="start" color="inherit" aria-label="menu" className="nav-logo">
@@ -21,8 +47,13 @@ const Navbar = () => {
                     <Button color="inherit" className="navbar-posts">
                         <Link to='/myposts' style={{ textDecoration: 'none', color: 'white' }}>View Posts
                         </Link></Button>
+                    <ClearAllIcon fontSize="large" style={{color: 'black'}} onClick={handleOpen} className={classes.navExpand}/>
                 </Toolbar>
             </AppBar>
+
+            {open ? <Sidebar toggle={handleOpen} /> : null}
+
+            </>
     );
 }
 export default Navbar;
---
+++ client/src/Components/Sidebar.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import { Grid, Typography } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+
+const useStyles = makeStyles(() => ({
+
+    wrapper: {
+        position: 'absolute',
+        left: '0',
+        top: '70px',
+        padding: '60px 5px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgb(42, 42, 68)',
+        width: '40%',
+        height: '100vh',
+        zIndex: '1000',
+        transitionTimingFunction: 'ease-in',
+        transition: '0.2s'
+    },
+    link: {
+        color: 'white',
+        margin: '40px'
+    },
+    close: {
+        position: 'absolute',
+        top: '0',
+        right: '0',
+        padding: '5px 5px',
+        '&:hover': {
+            cursor: 'pointer'
+        }
+    }
+
+}))
+
+const Sidebar = (props) => {
+    const classes = useStyles();
+    return (
+        <Grid container className={classes.wrapper}>
+            <Grid item className={classes.close}>
+                <CloseIcon fontSize="large" onClick={props.toggle} />
+            </Grid>
+            <Grid item>
+                <Link to="/" style={{ color: 'inherit', textDecoration: 'none' }}><Typography variant="h6" className={classes.link} onClick={props.toggle}>Home</Typography></Link>
+            </Grid>
+
+            <Grid item>
+                <Link to='/addpost' style={{ textDecoration: 'none', color: 'white' }}><Typography variant="h6" className={classes.link} onClick={props.toggle}>Add A Post</Typography></Link>
+            </Grid>
+            <Grid item>
+                <Link to='/myposts' style={{ textDecoration: 'none', color: 'white' }}><Typography variant="h6" className={classes.link} onClick={props.toggle}>View Posts</Typography></Link>
+            </Grid>
+        </Grid>
+    )
+}
+export default Sidebar;
